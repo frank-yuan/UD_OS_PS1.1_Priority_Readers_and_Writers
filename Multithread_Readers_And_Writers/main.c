9e8a874cd857f8a202451df7f21d5f93ec116e5f
frank-yuan <felk2005@gmail.com>
2016-11-20 11:16:01 -0400

    Initial Commit

---
+++ Multithread_Readers_And_Writers/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Multithread_Readers_And_Writers
+//
+//  Created by Xuan Yuan on 2016-11-20.
+//  Copyright © 2016 Frank Yuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
